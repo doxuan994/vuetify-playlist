c1928e7be6faa5816aca1f989789d085c95cb3c1
Xuan Do <doxuancntp@gmail.com>
2019-06-08 16:00:44 -0400

    Commit.

---
+++ todoninja/src/views/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dashboard">
+      <h1 class="subheading grey--text">Dashboard</h1>
+
+      <v-container class="my-5">
+        <v-card flat class="pa-3">
+          <v-layout row wrap>
+            <v-flex xs12 md6>
+              <div class="caption grey--text">Project Title</div>
+              <div class="">Create a new website</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Person</div>
+              <div>The Net Ninja</div>
+            </v-flex>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Due by</div>
+              <div>1st Jan 2019</div>
+            </v-flex>
+            <v-flex xs2 sm4 md2>
+              <div class="caption grey--text">Status</div>
+              <div>ongoing</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    },
+    components: {
+
+    }
+  }
+</script>
