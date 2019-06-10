75d32247522964f8a8531da1f4c241f21d14e3ed
Xuan Do <doxuancntp@gmail.com>
2019-06-10 12:34:55 -0400

    Commit.

--- todoninja/src/views/Dashboard.vue
+++ todoninja/src/views/Dashboard.vue
@@ -1,77 +1,90 @@
 <template>
   <div class="dashboard">
-      <h1 class="subheading grey--text">Dashboard</h1>
+    <h1 class="subheading grey--text">Dashboard</h1>
 
-      <v-container class="my-5">
-        <v-layout row wrap class="mb-3">
+    <v-container class="my-5">
+      <v-layout row wrap class="mb-3">
 
-          <v-tooltip top>
-            <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
-              <v-icon left small>folder</v-icon>
-              <span class="caption text-lowercase">By project name</span>
-            </v-btn>
-            <span>Sort projects by project name</span>
-          </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">By project name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
 
-          <v-tooltip top>
-            <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
-              <v-icon left small>person</v-icon>
-              <span class="caption text-lowercase">By person name</span>
-            </v-btn>
-              <span>Sort projects by person name</span>
-          </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">By person name</span>
+          </v-btn>
+          <span>Sort projects by person name</span>
+        </v-tooltip>
 
+      </v-layout>
+
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div class="">{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
+            </div>
+          </v-flex>
         </v-layout>
+        <v-divider></v-divider>
+      </v-card>
 
-        <v-card flat v-for="project in projects" :key="project.title">
-          <v-layout row wrap :class="`pa-3 project ${project.status}`">
-            <v-flex xs12 md6>
-              <div class="caption grey--text">Project Title</div>
-              <div class="">{{ project.title }}</div>
-            </v-flex>
-            <v-flex xs6 sm4 md2>
-              <div class="caption grey--text">Person</div>
-              <div>{{ project.person }}</div>
-            </v-flex>
-            <v-flex xs6 sm4 md2>
-              <div class="caption grey--text">Due by</div>
-              <div>{{ project.due }}</div>
-            </v-flex>
-            <v-flex xs2 sm4 md2>
-              <div class="right">
-                <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
-              </div>
-            </v-flex>
-          </v-layout>
-          <v-divider></v-divider>
-        </v-card>
-
-      </v-container>
+    </v-container>
 
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        projects: [
-          { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-          { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-          { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-          { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        ]
-      }
-    },
-    components: {
+import db from '@/fb';
 
-    },
-    methods: {
-      sortBy(prop) {
-        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1);
-      }
+
+export default {
+  data() {
+    return {
+      projects: [],
     }
+  },
+  components: {
+
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1);
+    }
+  },
+  created() {
+    db.collection('projects').onSnapshot(res => {
+      const changes = res.docChanges();
+
+      changes.forEach(change => {
+        if (change.type === 'added') {
+          this.projects.push({
+            ...change.doc.data(),
+            id: change.doc.id
+          })
+        }
+      })
+
+    })
   }
+}
 </script>
 
 
